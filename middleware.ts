45d743aaa9649051f7fee89a46c9b9933e5ebe90
marek3289 <marek2494@o2.pl>
2025-10-22 09:27:00 +0200

    update

--- middleware.ts
+++ middleware.ts
@@ -7,5 +7,4 @@ import oryConfig from "@/ory.config"
 // This function can be marked `async` if using `await` inside
 export const middleware = createOryMiddleware(oryConfig)
 
-// See "Matching Paths" below to learn more
 export const config = {}
